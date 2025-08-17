ab5fa89f65474f345d931845b325855daca60977
Taoshi <nicolaikh@live.dk>
2025-08-18 01:15:02 +0200

    Add some xp service stats to the console

--- SketchBot/Services/XpService.cs
+++ SketchBot/Services/XpService.cs
@@ -13,22 +13,41 @@ namespace Sketch_Bot.Services
 {
     public class XpService
     {
-        private readonly Timer _timer;
+        private Dictionary<string, Timer> timers = new Dictionary<string, Timer>();
         public List<SocketGuildUser> userlist = new List<SocketGuildUser>();
+        private List<ulong> userIds = new List<ulong>();
+        private int totalEventCount = 0;
+        private int uniqueEventCount = 0;
         public XpService(DiscordSocketClient client)
         {
-            _timer = new Timer(async _ =>
+            timers.Add("main xp loop", new Timer(async _ =>
             {
                 //Console.WriteLine(userlist.Count() + " users cleared!");
                 userlist.Clear();
             },
             null,
             TimeSpan.FromMinutes(1),
-            TimeSpan.FromMinutes(1));
+            TimeSpan.FromMinutes(1)));
+            timers.Add("event count display", new Timer(async _ =>
+            {
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss} - {totalEventCount} ({uniqueEventCount} unique) xp events in the last 15 minutes!");
+                totalEventCount = 0;
+                uniqueEventCount = 0;
+                userIds.Clear();
+            },
+            null,
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(15)));
         }
         public void addUser(SocketGuildUser user)
         {
             userlist.Add(user);
+            if (!userIds.Contains(user.Id))
+            {
+                userIds.Add(user.Id);
+                uniqueEventCount++;
+            }
+            totalEventCount++;
         }
         public List<SocketGuildUser> GetList()
         {
